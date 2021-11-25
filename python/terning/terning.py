8e1dedb4ba6913da9a267b854c28f06b803c4361
szcarr <67342876+szcarr@users.noreply.github.com>
2021-11-25 12:51:03 +0100

    Add files via upload

--- python/terning/terning.py
+++ python/terning/terning.py
@@ -5,70 +5,111 @@ import utime
 pins = [0, 1, 2, 3, 4, 5, 6]
 ledListe = []
 number = [
-    1,                          # 1 - 1 Auge
-    32 + 2,                     # 34 - 2 Auge
-    1 + 8 + 64,                 # 73 - 3 Auge
-    1 + 2 + 32 + 64,            # 99 - 4 Auge
+    8,                          # 8   - 1  Auge
+    1 + 32,                     # 33  - 2 Auge
+    2 + 8 + 64,                 # 74  - 3 Auge
+    1 + 2 + 32 + 64,            # 99  - 4 Auge
     1 + 2 + 8 + 32 + 64,        # 107 - 5 Auge
     1 + 2 + 4 + 16 + 32 + 64    # 119 - 6 Auge
     ]
 #
-#    *   *      1      2
-#    * * *      4  8  16
-#    *   *      32    64
+#    *   *      64     32
+#    * * *      16  8  4
+#    *   *       1     2
 #
 #
 #    *          1
 #      *           8  
 #        *            64
 
+'''
+----------------------------------------PATTERNS----------------------------------------
+'''
+
+lineRotate = [
+    1 + 8 + 32,
+    8,
+    2 + 8 + 64,
+    4 + 8 + 16,
+    32 + 8 + 1,
+    8,
+    64 + 8 + 2,
+    16 + 8 + 4,
+]
+
 valueToLed = {}
 
 def binToTerning(verdi):
     #Verdien er summen av alle leds forhold til posisjon
     for i in range(7):
-        mask = verdi & 1 << i
+        maske = verdi & 1 << i
         pinVal = False
-        if mask:
+        if maske:
             pinVal = True        
         ledListe[i].value(pinVal)
     
+def showFace(face):
+    binToTerning(number[face - 1])
 
-def setup():
-    for i in range(len(pins)):
-        ledListe.append(machine.Pin(pins[i], machine.Pin.OUT))
-        ledListe[i].value(0)
-
-
-def lystest():
-    for i in range(2):
-        print("")
-
-def asopdkaosdk():
+def iterateOverAllNumbersInDice():
     for i in range(len(number)):
         binToTerning(number[i])
-        utime.sleep(1)
+        utime.sleep(5)
 
-def rollRandomNumber():
-    return random.randrange(1, 7)
+def lineRotatePattern():
+    for y in range(len(lineRotate)):
+        binToTerning(lineRotate[y])
+        utime.sleep(0.3)
 
-def terningDisplay(numberToDisplay):
-    for i in range(len(ledListe)):
-        if numberToDisplay == 1:
-            print("")
-            
+def patternMode():
+    while True:
+        try:
+            print("Doing: showing all numbers on dice")
+            iterateOverAllNumbersInDice()
+            print("Doing: randomroll")
+            for i in range(6):
+                showFace(random.randrange(1, len(number)))
+                utime.sleep(0.3)
+            print("Doing: linerotate")
+            for i in range(3):
+                lineRotatePattern()
+        except KeyboardInterrupt:
+            print("Exiting...")
+            resetLED()
+            break
+'''
+----------------------------------------HELPERS----------------------------------------
+'''
+
 def resetLED():
     for i in range(len(ledListe)):
         ledListe[i].value(0)
 
 def addValuesToHashMap(valueToLed):
     for i in range(len(number)):
-        valueToLed[number[i]] = pins[i]
+        valueToLed[number[i]] = pins[i]  
+
+def setup():
+    for i in range(len(pins)):
+        ledListe.append(machine.Pin(pins[i], machine.Pin.OUT))
+        ledListe[i].value(0)      
+
 '''
---------------------MAIN--------------------
+----------------------------------------INITIALIZER----------------------------------------
 '''
 
 setup()
 addValuesToHashMap(valueToLed)
+
+'''
+----------------------------------------MAIN----------------------------------------
+'''
+
 #binToTerning(1 + 2 + 8 + 32 + 64)
-asopdkaosdk()
+#showFace(random.randrange(1, len(number)))
+#utime.sleep(3)
+#patternMode()
+#iterateOverAllNumbersInDice()
+for i in range(20):
+    lineRotatePattern()
+resetLED()
